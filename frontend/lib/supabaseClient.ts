446d1f8ead8827fa79e2d11ecff754e60cf2b180
Mahmood <mahmoodelahi33@gmail.com>
2026-02-14 19:08:20 -0700

    Added common supabase key

---
+++ frontend/lib/supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://kfkwgwxfhtgjipefigrg.supabase.co",
+  "sb_publishable_fO3gbsu0QS1P3m4cpPl1ZQ_fcfyR3LF"
+);
